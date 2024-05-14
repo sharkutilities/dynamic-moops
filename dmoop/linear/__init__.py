6c9e31a73a048d574a54744ace58cfd370b1780b
E33605 <debmalyapramanik.005@gmail.com>
2024-05-14 11:52:01 +0530

    ✨ register namespace for linear optimization techniques

---
+++ dmoop/linear/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+"""
+A Set of Optimization Approach involving Linear Objective Functions
+
+An function :attr:`f(x, y, ...)` is said to be linear if the degree of
+the polynomials are either zero or one. In case of two independent
+variables :attr:`f(x, y)` the graph is a straight line or in case of
+three variables :attr:`f(x, y, z)` it is a plane.
+"""
