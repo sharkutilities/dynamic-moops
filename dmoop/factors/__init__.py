01e88613426683f8da3e45cfcf78e8c7dcf6fd28
E33605 <debmalyapramanik.005@gmail.com>
2025-01-31 11:44:14 +0530

    📃 factor submodule usage notes

--- dmoop/factors/__init__.py
+++ dmoop/factors/__init__.py
@@ -2,6 +2,11 @@
 
 """
 A Set of Mathematical Factor/Ratio for N-Dimensional Optimization
+
+The defined ratio and their names are available to be used by any of
+the optimization algorithms. Typically, when the model is trained with
+the :func:`.fit()` method the same ratio is available as a class
+attribute from the called function - this gives more flexibility.
 """
 
 from dmoop.factors import linear
