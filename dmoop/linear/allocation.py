d28e5b35392dc1956f119b5c139ecaf23312e2ce
E33605 <debmalyapramanik.005@gmail.com>
2025-01-31 15:56:36 +0530

    ✨ define SimpleLinearOptimizer method

--- dmoop/linear/allocation.py
+++ dmoop/linear/allocation.py
@@ -12,7 +12,39 @@ either minimization of maximization of all variables individually.
 import numpy as np
 
 from typing import Iterable
+from dmoop.factors import linear
 from dmoop.base import BaseConstruct
+
+class SimpleLinearOptimizer(BaseConstruct):
+    """
+    Simple Multiple Objective Optimization
+
+    The most simple way to find a solution to a multi-objective is to
+    multiply all the features together, given maximization from the
+    features that needs minimizations.
+    """
+
+    def __init__(self, xs : Iterable, senses : Iterable, **kwargs) -> None:
+        super().__init__(
+            xs, senses,
+
+            # model name attribute defaults to class name
+            modelname = kwargs.get("modelname", None)
+        )
+
+
+    def fit(self) -> None:
+        """
+        The Simplistic Model for Linear N-Dimensional Allocation
+        """
+
+        self.alpha = linear.alpha(self.xs, self.senses)
+        return None
+
+
+    def predict(self) -> np.ndarray:
+        return self.alpha / self.alpha.sum()
+
 
 class SimpleLinearDeltaOptimizer(BaseConstruct):
     """
